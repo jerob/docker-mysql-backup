68255a9837871f789bd2a6f11c200bb6640f168c
Jérémie Robert <appydo+jerob@gmail.com>
2015-05-28 23:48:57 +0200

    Create directory

--- backup.sh
+++ backup.sh
@@ -2,6 +2,8 @@
 BACKUP=/backup
 NOW="$(date +"%d-%m-%Y_%H%M")"
 OLD="$(date +"%d-%m-%Y" -d '-7 day')"
+# create directory
+mkdir -p "$BACKUP/sql/$NOW/"
 # all databases
 mysqldump -u root --host=$MYSQL_PORT_3306_TCP_ADDR --protocol=$MYSQL_PORT_3306_TCP_PROTO --password=$MPASS --all-databases --single-transaction > "$BACKUP/sql/$NOW/all_databases.sql"
 # backup each base of the database
