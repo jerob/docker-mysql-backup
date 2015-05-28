d646ef2e2cde43f56364d5a4d14f7d16fd692471
Jérémie Robert <appydo+jerob@gmail.com>
2015-05-29 00:30:07 +0200

    Delete mkdir

--- backup.sh
+++ backup.sh
@@ -8,7 +8,6 @@ mkdir -p "$BACKUP/sql/$NOW/"
 mysqldump -u root --host=$MYSQL_PORT_3306_TCP_ADDR --protocol=$MYSQL_PORT_3306_TCP_PROTO --password=$MPASS --all-databases --single-transaction > "$BACKUP/sql/$NOW/all_databases.sql"
 # backup each base of the database
 DBS="$(mysql -u $MUSER --host=$MYSQL_PORT_3306_TCP_ADDR --protocol=$MYSQL_PORT_3306_TCP_PROTO -p$MPASS -Bse 'show databases')"
-mkdir "$BACKUP/sql/$NOW"
 for db in $DBS
 do
 	mysqldump -u root --host=$MYSQL_PORT_3306_TCP_ADDR --protocol=$MYSQL_PORT_3306_TCP_PROTO --password=$MPASS $db --single-transaction > "$BACKUP/sql/$NOW/$db.sql"
