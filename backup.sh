d33406ec28ab2c265aea90594db21d98075bac71
Jérémie Robert <appydo+jerob@gmail.com>
2015-05-28 23:47:26 +0200

    MPASS

--- backup.sh
+++ backup.sh
@@ -3,13 +3,13 @@ BACKUP=/backup
 NOW="$(date +"%d-%m-%Y_%H%M")"
 OLD="$(date +"%d-%m-%Y" -d '-7 day')"
 # all databases
-mysqldump -u root --host=$MYSQL_PORT_3306_TCP_ADDR --protocol=$MYSQL_PORT_3306_TCP_PROTO --password=$MYSQL_ENV_MYSQL_ROOT_PASSWORD --all-databases --single-transaction > "$BACKUP/sql/$NOW/all_databases.sql"
+mysqldump -u root --host=$MYSQL_PORT_3306_TCP_ADDR --protocol=$MYSQL_PORT_3306_TCP_PROTO --password=$MPASS --all-databases --single-transaction > "$BACKUP/sql/$NOW/all_databases.sql"
 # backup each base of the database
 DBS="$(mysql -u $MUSER -h $MHOST -p$MPASS -Bse 'show databases')"
 mkdir "$BACKUP/sql/$NOW"
 for db in $DBS
 do
-	mysqldump -u root --host=$MYSQL_PORT_3306_TCP_ADDR --protocol=$MYSQL_PORT_3306_TCP_PROTO --password=$MYSQL_ENV_MYSQL_ROOT_PASSWORD $db --single-transaction > "$BACKUP/sql/$NOW/$db.sql"
+	mysqldump -u root --host=$MYSQL_PORT_3306_TCP_ADDR --protocol=$MYSQL_PORT_3306_TCP_PROTO --password=$MPASS $db --single-transaction > "$BACKUP/sql/$NOW/$db.sql"
 done
 # delete old sql
 rm -r $BACKUP/sql/$OLD*
